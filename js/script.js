d0285d0c23ecb18f9364a0e7ed113ba319d3209c
osa1p <oidahor@u.rochester.edu>
2025-02-20 14:18:45 -0500

    Initial commit of portfolio website

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function typeEffect(elementId, text, speed=100) {
+    let index = 0;
+
+    function type(){
+        if (index < text.length) {
+            document.getElementById(elementId).innerHTML += text.charAt(index);
+            index++;
+            setTimeout(type, speed);
+        }
+    }
+    type();
+}
+
+window.onload = function () {
+    if (document.getElementById("Name")) {
+        typeEffect("Name", "I'm Osa Idahor", 100);
+    }
+
+    if (document.getElementById("Projects")) {
+        typeEffect("Projects", "My Projects", 100);
+    }
+};
+
+
+document.getElementById("download-resume").addEventListener("click", function() {
+    window.open("static/docs/Osa_Idahor_Resume.pdf", "_blank");
+});
+
+    
+
